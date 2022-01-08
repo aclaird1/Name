31830f81148106bd9239e87e8b229cc70cfee5f6
aclaird1 <97352510+aclaird1@users.noreply.github.com>
2022-01-08 07:56:41 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using system;
+
+	Global Class Sample
+	{
+		Void Main(string[], args)
+		{ 
+			// 1 add a string variable for your name 
+			String myName = GetName();
+
+			System.console.writeline("Hellow World my Name is: " + myName);
+
+		}
+		Public Static string GetName(),
+		{
+			String myName;
+			// 2 add a print statement t0 tell you enter your name
+			SPriystem.Console.WriteLine("Please Enter Your Name:");
+
+			// 3 Now the readline statement will read in your name
+			myName = Console.ReadLine();
+
+			return myName;
+		}
+	}
+
+
